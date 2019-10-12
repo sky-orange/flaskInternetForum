13f2bab6fe5ba5193958c03b166e277c31cc88ee
sky-orange <chariszr@gmail.com>
2019-10-12 14:13:09 +0100

    Refactor to avoid redirecting to blank page just to create new thread or comment

--- forum.py
+++ forum.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, redirect, request, url_for
 import json
+
 app = Flask(__name__)
 
 
+# Data storage functions
 def load_threads_data():
     global threads_data
     with open("threadsData.json") as json_data:
@@ -14,22 +16,30 @@ def save_threads_data():
         json.dump(threads_data, outfile, indent=2)
 
 
+# Startup
 try:
     load_threads_data()
 except IOError:
     threads_data = {}
 
 
-@app.route("/thread/<thread_number>/create-new-comment", methods=["POST"])
-def add_new_comment(thread_number):
-    global threads_data
-    threads_data[thread_number]["comments"].append(request.form.to_dict())
-    save_threads_data()
-    return redirect("thread/" + thread_number)
+# Routes
+@app.route("/", methods=["GET", "POST"])
+def main_page():
+    if request.method == "POST":
+        return create_thread()
+    return render_template("mainPage.html", threads=threads_data)
 
 
-@app.route("/create-new-thread", methods=["POST"])
-def create_new_thread():
+@app.route("/thread/<thread_number>/", methods=["GET", "POST"])
+def thread_page(thread_number):
+    if request.method == "POST":
+        create_thread_comment(thread_number)
+    return render_template("threadPage.html", thread=threads_data[thread_number])
+
+
+# Functions to process user input
+def create_thread():
     global threads_data
     new_thread_number = str(len(threads_data))
     threads_data[new_thread_number] = request.form.to_dict()
@@ -38,11 +48,7 @@ def create_new_thread():
     return redirect(url_for("thread_page", thread_number=new_thread_number))
 
 
-@app.route("/")
-def main_page():
-    return render_template("mainPage.html", threads=threads_data)
-
-
-@app.route("/thread/<thread_number>/", methods=["GET"])
-def thread_page(thread_number):
-    return render_template("threadPage.html", thread=threads_data[thread_number])
+def create_thread_comment(thread_number):
+    global threads_data
+    threads_data[thread_number]["comments"].append(request.form.to_dict())
+    save_threads_data()
